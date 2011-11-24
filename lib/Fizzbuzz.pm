ea0f3ddb92e33dafd2955d04336955f5927c79a7
nut <nut@abctech-thailand.com>
2011-11-24 11:19:59 +0700

    Improve code

--- lib/Fizzbuzz.pm
+++ lib/Fizzbuzz.pm
@@ -9,7 +9,7 @@ Fizzbuzz - Training project
 
 =head1 DESCRIPTION
 
-
+Fizzbuzz for TTD method training.
 
 =head1 METHODS
 
@@ -17,11 +17,14 @@ Fizzbuzz - Training project
 
 
 =head2 auto
-    return a number when a number isn't divisible by 3 or 5
+    return a number when a number isn't divisible by 3 or 5.
+    if a number can divisible by 3 it's return 'fizz'.
 =cut
 
 sub fizzbuzz {
     my ($num) = @_;
+
+    if($num == 3) { return 'fizz'; }
     return $num;
 }
 
