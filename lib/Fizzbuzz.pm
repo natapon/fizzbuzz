4e812c060224dee2645301b13aa7ec8f4ca099f2
nut <nut@abctech-thailand.com>
2011-11-24 11:07:47 +0700

    Add POD and Fix critic blame

--- lib/Fizzbuzz.pm
+++ lib/Fizzbuzz.pm
@@ -1,5 +1,25 @@
 package Fizzbuzz;
 
+use strict;
+use warnings;
+
+=head1 NAME
+
+Fizzbuzz - Training project
+
+=head1 DESCRIPTION
+
+
+
+=head1 METHODS
+
+=cut
+
+
+=head2 auto
+    return a number when a number isn't divisible by 3 or 5
+=cut
+
 sub fizzbuzz {
     my ($num) = @_;
     return $num;
